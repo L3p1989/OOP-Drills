542b0a0790b930ebb7f9f3e545f6c45db8369da0
Patrick Hogan <phogan110@live.com>
2018-11-06 07:39:52 -0700

    added `Truck` class

--- scripts.js
+++ scripts.js
@@ -47,11 +47,11 @@ function Person(name, age, state) {
     this.name = name;
     this.age = age;
     this.state = state;
-};
+};//class constructor for Person
 
 sayHello = function(object) {
     console.log(`Hey! My name is ${object.name} I am ${object.age} years old and I live in ${object.state}.`);
-}
+};//logs string to console
 
 person1 = new Person('Noreen', 50, 'Arizona');
 sayHello(person1);
@@ -73,24 +73,49 @@ class Family {
         this.name = name;
         this.city = city;
         this.age = age;
+    };
+    sayHello() {
+        console.log(`Hey! My name is ${this.name} I am ${this.age} years old and I live in ${this.city}.`);
+    };
+};//ES6 class
+
+person1 = new Family('Noreen', 'Chandler', 50);
+person1.sayHello();
+
+person2 = new Family('Shannon', 'Denver', 33);
+person2.sayHello();
+
+person3 = new Family('Steph', 'Denver', 27);
+person3.sayHello();
+
+person4 = new Family('Tristan', 'Colorado Springs', 15);
+person4.sayHello();
+
+person5 = new Family('Gavine', 'Los Angeles', 18);
+person5.sayHello();
+
+class Vehicle {
+    constructor(manufacturer, wheels) {
+        this.manufacturer = manufacturer;
+        this.wheels = wheels;
+    }
+    aboutVehicle() {
+            console.log(`This is a vehicle, made by ${this.manufacturer}, that has ${this.wheels} wheels.`)
+    };
+};
+
+let truckBed = false;
+
+class Truck extends Vehicle {
+    constructor(manufacturer, wheels, doors, hasBed) {
+        super(manufacturer, wheels);
+        this.doors = doors;
+        Boolean = hasBed;
+    }
+    aboutVehicle() {
+        console.log(`This is a truck, made by ${this.manufacturer}, that has ${this.wheels} wheels, ${this.doors} doors, and a truck bed.`)
     }
 };
 
-sayHello = function(Family) {
-    console.log(`Hey! My name is ${Family.name} I am ${Family.age} years old and I live in ${Family.city}.`);
-}
-
-person1 = new Family('Noreen', 'Chandler', 50);
-sayHello(person1);
-
-person2 = new Family('Shannon', 'Denver', 33);
-sayHello(person2);
-
-person3 = new Family('Steph', 'Denver', 27);
-sayHello(person3)
-
-person4 = new Family('Tristan', 'Colorado Springs', 15);
-sayHello(person4);
-
-person5 = new Family('Gavine', 'Los Angeles', 18);
-sayHello(person5);
+let colorado = new Truck('Chevy', 4, 4, true);
+colorado.aboutVehicle();
